11845c7607aaa8e9e0e546c6aba3c3d0d577e539
Iryna Rudenko <irinka.rude@gmail.com>
2023-05-30 22:43:49 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,9 +4,8 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
-// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
 function makeGallery(items) {
-  return galleryItems.map({preview, original, description} => {
+  return galleryItems.map({preview, original, description} => 
       `<li class="gallery__item">
   <a class="gallery__link" href=${original}>
     <img
@@ -17,7 +16,7 @@ function makeGallery(items) {
     />
   </a>
 </li>`
-    })
+    )
     .join("")
   }
 const galleryList = document.querySelector('.gallery');
@@ -26,14 +25,6 @@ const galleryCards = makeGallery(galleryItems);
 galleryList.innerHTML = galleryCards;
 galleryList.addEventListener('click', onPictureClick) ;
 
-
-// Реалізація делегування на ul.gallery і отримання url великого зображення.
-// Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. 
-// Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані(.min) файли бібліотеки.
-// 
-// Відкриття модального вікна по кліку на елементі галереї.Для цього ознайомся з документацією і прикладами.
-// Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. 
-Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
 function onPictureClick(event) {
   event.preventDefault();
 
@@ -46,9 +37,6 @@ function onPictureClick(event) {
     
     onModalOpen (bigPictureLink);
     }
-
-// Посилання на оригінальне зображення повинно зберігатися в data - атрибуті source на елементі < img >,
-//     і вказуватися в href посилання.Не додавай інші HTML теги або CSS класи, крім тих, що містяться в цьому шаблоні.
 
 
 function onModalOpen(url) {
@@ -66,13 +54,4 @@ function onModalOpen(url) {
         });
         bigImage.show();
 }  
-
-    // Зверни увагу на те, що зображення обгорнуте посиланням,
-    // отже по кліку за замовчуванням користувач буде перенаправлений на іншу сторінку.
-    //     Заборони цю поведінку за замовчуванням.
-            
-            
-    //         Додай закриття модального вікна після натискання клавіші Escape.
-    //         Зроби так, щоб прослуховування клавіатури було тільки доти, доки відкрите модальне вікно. 
-    //         Бібліотека basicLightbox містить метод для програмного закриття модального вікна.
 
